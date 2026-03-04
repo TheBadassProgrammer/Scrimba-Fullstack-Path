8acdb66116042a1aef4889f573ed4f75395da379
Nitin Tripathi <nitstrip001@gmail.com>
2026-03-04 23:20:20 +0530

    display remaining cards

--- War/index.js
+++ War/index.js
@@ -2,17 +2,27 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id
+            console.log(deckId)
         })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
 
+/**
+ * Challenge:
+ * 
+ * Display the number of cards remaining in the deck on the page
+ * Hint: Check the data that comes back when we draw 2 new cards
+ * to see if there's anything helpful there for this task (😉)
+ */
+let remaining = 52
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
@@ -23,67 +33,24 @@ drawCardBtn.addEventListener("click", () => {
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
-            war(data.cards[0], data.cards[1])
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+            document.getElementById("remaining").innerText = `Remaining: ${data.remaining}`
         })
 })
-/**
- * Challenge:
- * 
- * Try to determine which of the 2 cards is the "winner" (has higher value)
- * Aces are the card with the highest "score"
- * 
- * In parts:
- * 
- * 1. Create a function that takes 2 card objects as parameters, 
- * `card1` and `card2`. These card objects have a property called
- * `value`, which can be any one of the following strings, in
- * order of rising "score":
- * 
- * "2", "3", "4", "5", "6", "7", "8", "9", 
- * "10", "JACK", "QUEEN", "KING", "ACE"
- * 
- * I.e. "2" is the lowest score and "ACE" is the highest.
- * 
- * The function should determine which of the 2 cards (`card1`
- * or `card2`) has the higher score, or if they have the same score.
- * 
- * Log which card wins (or "It's a tie!" 
- * if they're the same) to the console
- */
 
-function war(card1, card2){
-    let score1 = 0
-    let score2 = 0
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
     
-    if(card1.value in ["2","3","4","5","6","7","8","9","10"]){
-        score1 = parseInt(card1.value, 10)
-    } else if(card1.value == "JACK") {
-        score1 = 11
-    } else if(card1.value == "QUEEN") {
-        score1 = 12
-    } else if(card1.value == "KING") {
-        score1 = 13
-    } else if(card1.value == "ACE") {
-        score1 = 14
-    }
-    
-    if(card2.value in ["2","3","4","5","6","7","8","9","10"]){
-        score2 = parseInt(card2.value, 10)
-    } else if(card2.value == "JACK") {
-        score2 = 11
-    } else if(card2.value == "QUEEN") {
-        score2 = 12
-    } else if(card2.value == "KING") {
-        score2 = 13
-    } else if(card2.value == "ACE") {
-        score2 = 14
-    }
-    
-    if(score1 > score2){
-        console.log("card1 won")
-    } else if (score2 > score1){
-        console.log("card2 won")
-    } else{
-        console.log("It's a tie!")
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "Card 2 wins!"
+    } else {
+        return "War!"
     }
 }
+

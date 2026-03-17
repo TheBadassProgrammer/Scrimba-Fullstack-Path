73c06c4b647bf9597405dc652e1351dccb43b63b
Nitin Tripathi <nitstrip001@gmail.com>
2026-03-18 01:24:58 +0530

    added a static movie card

--- Scrimba-Solo-Projects/Movie Watchlist/script.js
+++ Scrimba-Solo-Projects/Movie Watchlist/script.js
@@ -1,7 +1,18 @@
-async function searchMovie(){
-    const response = await fetch("http://www.omdbapi.com/?t=interstellar&apikey=ba2d466e")
+const searchBtn = document.getElementById("search-btn")
+const searchBox = document.getElementById("search-txt")
+
+searchBtn.addEventListener("click", async () => {
+    const searchTxt = searchBox.value
+    const data = await searchMovie(searchTxt)
+    displayMovies(data)
+})
+
+async function searchMovie(searchTxt){
+    const response = await fetch(`http://www.omdbapi.com/?t=${searchTxt}&apikey=ba2d466e`)
     const data = await response.json()
-    console.log(data)
+    return data
 }
 
-searchMovie()
+function displayMovies(movie) {
+    console.log(movie.Title)
+}

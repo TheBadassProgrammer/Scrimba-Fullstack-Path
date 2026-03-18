24145aa21e83bd0d399ad87c9565aa1f448bdb25
Nitin Tripathi <nitstrip001@gmail.com>
2026-03-18 12:34:35 +0530

    dynamic movie card

--- Scrimba-Solo-Projects/Movie Watchlist/script.js
+++ Scrimba-Solo-Projects/Movie Watchlist/script.js
@@ -3,6 +3,7 @@ const searchBox = document.getElementById("search-txt")
 
 searchBtn.addEventListener("click", async () => {
     const searchTxt = searchBox.value
+    console.log(searchTxt)
     const data = await searchMovie(searchTxt)
     displayMovies(data)
 })
@@ -15,4 +16,25 @@ async function searchMovie(searchTxt){
 
 function displayMovies(movie) {
     console.log(movie.Title)
+    const html = `
+    <div class="films-container">
+            <div class="movie">
+                <img id="movie-img" src="${movie.Poster}" alt="">
+                <div class="movie-info">
+                    <div class="title-stars">
+                        <h2>${movie.Title}</h2>
+                        <img id="star-icon" src="images/star-icon.png">
+                        <h3 class="rating">${movie.imdbRating}</h3>
+                    </div>
+                    <div class="runtime-genre-watchlist">
+                        <h3>${movie.Runtime}</h3>
+                        <h3>${movie.Genre}</h3>
+                        <a id="add-watchlist-btn" href="watchlist.html"><img src="images/plus-icon.png"> Watchlist</a>
+                    </div>
+                    <p class="plot">${movie.Plot}</p>
+                </div>
+            </div>
+        </div>
+    `
+    document.getElementById("films-container").innerHTML = html
 }

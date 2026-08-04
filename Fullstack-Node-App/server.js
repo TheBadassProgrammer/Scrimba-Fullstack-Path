0182324b6ef132c9c7c8dc36333555727eceb1ca
TheBadassProgrammer <nitstrip001@gmail.com>
2026-08-04 20:38:34 +0530

    wiring up the API

--- Fullstack-Node-App/server.js
+++ Fullstack-Node-App/server.js
@@ -1,14 +1,28 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
+import { handleGet } from "./handlers/routeHandlers.js"
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
-console.log(await getData())
-
 const server = http.createServer(async (req, res) => {
-    await serveStatic(req, res, __dirname)
+    if (req.url.startsWith('/api')) {
+        if (req.method == "GET") {
+            await handleGet(res)
+        }
+
+    }
+/*
+Challenge: 
+   1. Set up a route for ‘/api’.
+   2. Nest an if to check if the method is ‘GET’. 
+   3. When a GET request is received to '/api', use handleGet() to handle it.
+*/ 
+    else if (!req.url.startsWith('/api')) {
+        return await serveStatic(req, res, __dirname) 
+    }
 }) 
 
 server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
+ 

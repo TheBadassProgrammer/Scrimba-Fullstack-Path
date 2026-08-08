812dc2f4e2de29a4e2f869171ad7902a8d21f100
TheBadassProgrammer <nitstrip001@gmail.com>
2026-08-08 23:34:01 +0530

    incoming data sanitization

--- Fullstack-Node-App/handlers/routeHandlers.js
+++ Fullstack-Node-App/handlers/routeHandlers.js
@@ -2,6 +2,7 @@ import { getData } from '../utils/getData.js'
 import { sendResponse } from '../utils/sendResponse.js'
 import { parseJSONBody } from '../utils/parseJSONBody.js'
 import { addNewSighting } from '../utils/addNewSighting.js'
+import sanitizeHtml from 'sanitize-html';
 
 export async function handleGet(res) {
   const data = await getData()
@@ -13,7 +14,13 @@ export async function handlePost(req, res) {
 
   try {
     const parsedBody = await parseJSONBody(req)
-    await addNewSighting(parsedBody)
+    const sanitizedBody = Object.fromEntries(
+      Object.entries(parsedBody).map(([key, value]) => [key, sanitizeHtml(value, {
+        allowedTags: ['b'],
+        allowedAttributes: []
+        })])    
+    )
+    await addNewSighting(sanitizedBody)
     sendResponse(res, 201, 'application/json', JSON.stringify(parsedBody))
   } catch (err) {
     sendResponse(res, 400, 'application/json', JSON.stringify({error: err}))
